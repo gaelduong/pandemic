f5dfb34e4732c6de1a42392cbdc9faccbb2abb5c
gaelduong <31674048+gaelduong@users.noreply.github.com>
2018-03-07 20:51:54 -0500

    Add files via upload

---
+++ src/pandemic/views/DyeImage.java
@@ -0,0 +1,63 @@
+package pandemic.views;
+
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.swing.*;
+import javax.imageio.*;
+
+class DyeImage
+{
+    public static void main(String args[])
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    new DyeImage();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    public DyeImage() throws Exception
+    {
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ImageIcon img = new ImageIcon(GUI.class.getResource("/pandemic/resources/blueC.png"));
+        Image image = img.getImage();
+        JPanel panel = new JPanel(new GridLayout(1,0));
+        panel.add(new JLabel(new ImageIcon(image)));
+        panel.add(new JLabel(new ImageIcon(dye(image, new Color(255,0,0,128)))));
+        panel.add(new JLabel(new ImageIcon(dye(image, new Color(255,0,0,32)))));
+        panel.add(new JLabel(new ImageIcon(dye(image, new Color(0,128,0,32)))));
+        panel.add(new JLabel(new ImageIcon(dye(image, new Color(0,0,255,32)))));
+        f.getContentPane().add(panel);
+        f.pack();
+        f.setVisible(true);
+    }
+
+
+    public static Image dye(Image image, Color color)
+    {
+        int w = image.getWidth(null);
+        int h = image.getHeight(null);
+        BufferedImage dyed = new BufferedImage(w,h,BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = dyed.createGraphics();
+        g.drawImage(image, 0,0, null);
+        g.setComposite(AlphaComposite.SrcAtop);
+        g.setColor(color);
+        g.fillRect(0, 0,980, 500);
+        g.dispose();
+        return dyed;
+    }
+
+}
---
+++ src/pandemic/views/LaunchGUI.java
@@ -0,0 +1,25 @@
+package pandemic.views;
+
+import java.awt.EventQueue;
+
+public class LaunchGUI{
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+					
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
