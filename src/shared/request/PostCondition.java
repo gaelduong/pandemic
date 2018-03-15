73c53b224fcfffa5e6919940ab3044058a2b4d10
Russell <russell.katz@mail.mcgill.ca>
2018-03-15 17:30:26 -0400

    completed the core commands that client can request the server to do

--- src/shared/request/PostCondition.java
+++ src/shared/request/PostCondition.java
@@ -16,20 +16,57 @@ public class PostCondition {
      */
     public enum ACTION {
         /**
-         * CardSource source, CardSource destination
+         * This action moves a card from the source to destination.
+         * This action can emulate drawing cards, discarding cards, sharing cards, etc.
+         * You may use a null value for the PlayerCardSimple if you do not know the card (such as if you are drawing a card from the deck).
+         *
+         * <br><br><b>Arguments required:</b><br>
+         * {@link shared.PlayerCardSimple} card, String source, String destination
+         * <br><br>
+         * If the source or destination is a player, then it should be the {@link pandemic.RoleType#name()} of that player,
+         * Otherwise, use the {@link shared.CardTargetType#name()}.
          */
         MOVE_CARD, //from hand to discard pile, from deck to hand, from  player to player
 
         /**
-         * String playerUserName, String cityName,
+         * This action moves a players position to a city.
+         * <br>
+         * The server will make sure the request is valid before execution.
+         *
+         * <br><br><b>Arguments required:</b><br>
+         * String playerUserName, String cityName, {@link shared.TravelType travelType}
          */
-        MOVE_PLAYER_POS, //drive/ferry, direct flight, shuttle flight
+        MOVE_PLAYER_POS,
 
+        /**
+         * This action does the treat disease action in the player's current city.
+         * <br>
+         * The server will handle removing 1 cube or all depending on if the disease was cured.
+         *
+         * <br><br><b>Arguments required:</b><br>
+         * {@link pandemic.DiseaseType} diseaseType
+         */
+        TREAT_DISEASE,
 
-        TREAT_DISEASE,  //type of disease
+        /**
+         * This action discovers the cure for a certain DiseaseType and discards the inputted list of cards.
+         * <br>
+         * The server will check the player curing the disease is at a ResearchStation.
+         *
+         * <br><br><b>Arguments required:</b><br>
+         * {@link pandemic.DiseaseType} diseaseType, List<{@link shared.PlayerCardSimple}> cardsToDiscard
+         */
+        DISCOVER_CURE,
 
-
-        DISCOVER_CURE,  //type of disease
+        /**
+         * This action builds a research station at the player's current position and discards cards if necessary.
+         * <br>
+         * The server will handle discarding cards if the player's role is not OperationsExpert.
+         * The server will handle the fact that there can only be at most 6 stations on the board.
+         *
+         * <br><br><b>Arguments required:</b> NONE
+         */
+        BUILD_RESEARCH_STATION,
     }
 
     public PostCondition(ACTION action, Object... arguments) {
