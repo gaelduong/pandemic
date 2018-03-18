56d904a830fab0403f2054f17dd4670f2cb77df5
rkatz001 <russellkatz@outlook.com>
2018-03-18 01:20:30 -0400

    completed Consent Requiring actions

--- src/server/PandemicServer.java
+++ src/server/PandemicServer.java
@@ -4,22 +4,31 @@ import api.socketcomm.Server;
 import api.socketcomm.SocketBundle;
 import client.ClientCommands;
 import pandemic.Game;
-import shared.GameState;
 import shared.Utils;
 import shared.request.UpdateRequest;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Semaphore;
 
 public class PandemicServer extends Server {
     private final Map<SocketBundle, String> clientMap;    //<socket, playerUserName>
-    private final Game game;
+    private final Map<String, UpdateRequest> consentRequestMap; //<username, UR>
+    private Game game;
 
-    public PandemicServer(Game game, int port) throws IOException {
+    private Semaphore updateRequestSemaphore; //makes sure we only execute one UR at a time
+
+    public PandemicServer(int port) throws IOException {
         super(port);
-        this.clientMap = new HashMap<>();
+        this.clientMap = Collections.synchronizedMap(new HashMap<>());
+        this.consentRequestMap = Collections.synchronizedMap(new HashMap<>());
+        this.updateRequestSemaphore = new Semaphore(1);
+    }
+
+    public void setGame(Game game) {
         this.game = game;
     }
 
@@ -33,24 +42,15 @@ public class PandemicServer extends Server {
 
         switch (command) {
             case ANSWER_CONSENT_PROMPT:
-                //TODO russell will fill this out
-
+                answerConsentPrompt(client, message);
                 break;
 
             case SEND_UPDATE_REQUEST:
-                final String playerUsername = clientMap.get(client);
-                if (playerUsername == null) {
-                    System.out.printf("No player registered from %s! ERROR!\n", client.getSocket().getRemoteSocketAddress().toString());
-                    return;
-                }
-
-                final UpdateRequest updateRequest = (UpdateRequest)message.get(1);
-                executeRequestAndPropagate(playerUsername, game, updateRequest);
+                sendUpdateRequest(client, message);
                 break;
 
             case INITIATE_CONSENT_REQUIRING_MOVE:
-                //TODO russell will fill this out
-
+                initiateConsentReqMove(message);
                 break;
 
             case REGISTER_USERNAME:
@@ -61,13 +61,77 @@ public class PandemicServer extends Server {
         }
     }
 
-    private void executeRequestAndPropagate(String playerUsername, Game g, UpdateRequest updateRequest) {
+    private void answerConsentPrompt(SocketBundle client, List<Object> message) {
+        final String playerUsername = clientMap.get(client);
+        if (playerUsername == null) {
+            System.err.printf("No player registered from %s! ERROR!\n", client.getSocket().getRemoteSocketAddress().toString());
+            return;
+        }
+
+        final boolean acceptedRequest = (Boolean) message.get(1);
+        if (consentRequestMap.containsKey(playerUsername) && consentRequestMap.get(playerUsername) != null) {
+            if (acceptedRequest) {
+                System.out.printf("Player %s accepted the consent request!\n", playerUsername);
+                executeRequestAndPropagate(playerUsername, game, consentRequestMap.get(playerUsername));
+            }
+            consentRequestMap.put(playerUsername, null);
+        }
+    }
+
+    private void initiateConsentReqMove(List<Object> message) {
+        final String targetPlayerUsername = (String) message.get(1);
+        final String consentPrompt = (String) message.get(2);
+        final UpdateRequest consentUR = (UpdateRequest) message.get(3);
+
+        final SocketBundle targetPlayerSocket;
+        synchronized (clientMap) {
+            targetPlayerSocket = clientMap.entrySet().stream()
+                    .filter(e -> e.getValue().equals(targetPlayerUsername)).map(Map.Entry::getKey).findFirst().orElse(null);
+        }
+
+        if (targetPlayerSocket == null) {
+            System.err.printf("Unable to find corresponding socket connection for %s\n", targetPlayerUsername);
+            return;
+        }
+
+        Server.sendMessage(targetPlayerSocket, ClientCommands.RECEIVE_CONSENT_REQUEST.name(), consentPrompt);
+        consentRequestMap.put(targetPlayerUsername, consentUR);
+    }
+
+    private void sendUpdateRequest(SocketBundle client, List<Object> message) {
+        final String playerUsername = clientMap.get(client);
+        if (playerUsername == null) {
+            System.err.printf("No player registered from %s! ERROR!\n", client.getSocket().getRemoteSocketAddress().toString());
+            return;
+        }
+
+        if (!playerUsername.equals(game.getCurrentPlayer().getPlayerUserName())) {
+            System.err.printf("Rejected UpdateRequest from player %s, not your turn!\n", playerUsername);
+            return;
+        }
+
+        final UpdateRequest updateRequest = (UpdateRequest)message.get(1);
+        executeRequestAndPropagate(playerUsername, game, updateRequest);
+    }
+
+    private boolean executeRequestAndPropagate(String playerUsername, Game g, UpdateRequest updateRequest) {
+        boolean ret = false;
+
+        try {
+            updateRequestSemaphore.acquire();
+        } catch (InterruptedException ignore) {}    //we don't interrupt
+
         if (updateRequest.isRequestValid()) {
             updateRequest.executeRequest(game, playerUsername);
             sendMessageToClients(ClientCommands.RECEIVE_UPDATED_GAMESTATE.name(), g.generateCondensedGameState());
+            ret = true;
         } else {
             System.out.println("Could not satisfy update request from player " + playerUsername);
         }
+
+        updateRequestSemaphore.release();
+
+        return ret;
     }
 
     @Override
