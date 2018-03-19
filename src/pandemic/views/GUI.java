c4541cc4d9a1ff2cea6f4516f45a082328915057
rkatz001 <russellkatz@outlook.com>
2018-03-19 11:50:59 -0400

    remove null check

--- src/pandemic/views/GUI.java
+++ src/pandemic/views/GUI.java
@@ -833,7 +833,6 @@ public class GUI extends JFrame
 
 		/*----------Set up 8 buttons----------*/
 
-		if (gs == null) return;
 		String currentPlayer = gs.getCurrentPlayer();
 		if(currentPlayer != null && username.equals(currentPlayer) && gs.getCurrentPlayerActionsRemaining() > 0) {
 			//Drive Ferry
