acd3e65edd38b06c43e0ec5ae74157ef0a4d50c7
Russell <russell.katz@mail.mcgill.ca>
2018-04-13 11:54:27 -0400

    made MessageType more informative regarding game state

--- src/shared/MessageType.java
+++ src/shared/MessageType.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 
 public enum MessageType implements Serializable {
     INFORMATION,
-    WARNING,
-    ERROR,
     GAME_WON,
     GAME_LOST,
-    DISCARD_CARD;
+    DISCARD_CARD
 }
